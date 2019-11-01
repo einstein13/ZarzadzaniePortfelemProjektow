1306a3bd3307783b10b580b946fac76f98c6500e
Jakub Rak <jakub.r.rak@gmail.com>
2019-11-01 18:48:24 +0100

    pages app and requirements

---
+++ pages/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class HtmlContent(models.Model):
+
+    keyword = models.CharField(max_length=31)
+    content = models.TextField(null=True, blank=True)
+        
