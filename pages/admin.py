d6187f4ec5eadec230794c06dc629e93360cbc1e
Jakub Rak <jakub.r.rak@gmail.com>
2019-11-03 22:46:12 +0100

    Adding WYSIWYG

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .forms import HtmlContentForm
+from .models import HtmlContent
+
+class HtmlContentAdmin(admin.ModelAdmin):
+    # this is class replacing the form to WYSIWYG one
+    form = HtmlContentForm
+
+admin.site.register(HtmlContent, HtmlContentAdmin)
